df43048dbf6b61fd59a0eece3dee8e021a23cead
yangsong <390142688@qq.com>
2018-12-02 10:38:14 +0800

    TODOListViewController 完成DataSource和delegate方法

--- TODO/ViewController.swift
+++ TODO/ViewController.swift
@@ -8,11 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["购买水杯","吃药","修改密码"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView,cellForRowAt indexPath:IndexPath)->UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+       
+       if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+       {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else
+       {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+         tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
